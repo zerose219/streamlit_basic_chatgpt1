7408a2b4c376c144be811a7e29fa7dc090627ca7
zerose <zerose219@gmail.com>
2025-12-16 11:24:31 +0900

    fix:화면수정중

--- chap03/sec03/streamlit_basic.py
+++ chap03/sec03/streamlit_basic.py
@@ -13,26 +13,30 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("세영")
+page = st.sidebar.selectbox("페이지 선택", ["Chat","About"])
 
-# (1) st.session_state에 "messages"가 없으면 초기값을 설정
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-# (2) 대화 기록을 출력
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+if page=="Chat":
+    st.title("세영")
 
-# (3) 사용자 입력을 받아 대화 기록에 추가하고 AI 응답을 생성
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
+    # (1) st.session_state에 "messages"가 없으면 초기값을 설정
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
-    client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt}) 
-    st.chat_message("user").write(prompt) 
-    response = client.chat.completions.create(model="gpt-4o", messages=st.session_state.messages) 
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg}) 
-    st.chat_message("assistant").write(msg)
+    # (2) 대화 기록을 출력
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    # (3) 사용자 입력을 받아 대화 기록에 추가하고 AI 응답을 생성
+    if prompt := st.chat_input():
+        if not openai_api_key:
+            st.info("Please add your OpenAI API key to continue.")
+            st.stop()
+
+        client = OpenAI(api_key=openai_api_key)
+        st.session_state.messages.append({"role": "user", "content": prompt}) 
+        st.chat_message("user").write(prompt) 
+        response = client.chat.completions.create(model="gpt-4o", messages=st.session_state.messages) 
+        msg = response.choices[0].message.content
+        st.session_state.messages.append({"role": "assistant", "content": msg}) 
+        st.chat_message("assistant").write(msg)
